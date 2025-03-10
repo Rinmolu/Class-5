26bdf99e8ffb4181a71c2b278595e7f6d4cbefe8
Rinmolu <rinmoluadewumi@gmail.com>
2025-03-10 19:45:17 +0000

    Commit

---
+++ Activity2.py
@@ -0,0 +1,6 @@
+actual_cost=float(input("Enter The Cost."))
+selling_amount=float(input("Enter The Selling Amount."))
+if selling_amount > actual_cost:
+    print("You Have Made Profit!")
+else:
+    print("You Have Made A Loss!")
